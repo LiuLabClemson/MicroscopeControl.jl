ef5788a4e05209fc158033f430c0749c4f24c9cf
domweir <djw514@gmail.com>
2026-06-09 17:20:55 -0400

    added comments to test_slm

--- dev/test_slm.jl
+++ dev/test_slm.jl
@@ -3,7 +3,7 @@ using MicroscopeControl
 using MicroscopeControl.HardwareImplementations.ThorCamCSC
 using ImageView
 
-using MicroscopeControl.HardwareImplementations.Meadowlark
+using MicroscopeControl.HardwareImplementations.Meadowlark # for the SLM
 
 slm = Meadowlark.MLSLM()
 # Create SDK
@@ -13,22 +13,24 @@ Meadowlark.loadlut(lut_path)
 
 isvert = true
 period = 32
-Meadowlark.genblazed!(slm, isvert, period, blaze_start=1, blaze_end=1024)
-slm.phase = zeros(slm.height, slm.width) .+ 1.0/255
-Meadowlark.writesingleimage(slm)
+Meadowlark.genblazed!(slm, isvert, period, blaze_start=1, blaze_end=1024) #will need to replace this with a function for a checkerboard pattern
+#slm.phase = zeros(slm.height, slm.width) .+ 1.0/255
+Meadowlark.writesingleimage(slm) # returns 1 for success
 
-imshow(slm.phase)
+imshow(slm.phase') #Transposing the phase pattern so it is displayed correctly, since the SLM uses column major order (double check this)
 
 
 test_cam = ThorCamCSC.ThorCamCSCCamera()
 
-ThorCamCSC.gui(test_cam)
+ThorCamCSC.gui(test_cam) # Opens the camera GUI, which allows you to set the exposure time, gain, and other settings. The camera must be armed in order to capture an image, so this is a good way to set up the camera before capturing.
 
-test_cam.exposure_time = Clonglong(10000.0) # us
+test_cam.exposure_time = Clonglong(10000.0) # microseconds, so this is 10 ms
+
+
 
 data = ThorCamCSC.capture(test_cam)
 
-ThorCamCSC.disarmcamera(test_cam)   
+ThorCamCSC.disarmcamera(test_cam) #Disarming the camera allows for the next capture to be taken, otherwise the camera is still waiting for the next trigger and will not capture a new image
 
 imshow(Float64.(data))
 
@@ -58,9 +60,8 @@ imshow(imgs)
 #Close Camera
 ThorCamCSC.closecamera(test_cam)
 
-#Delete SDK
+#Delete Camera SDK
 ThorCamCSC.thorcamsdkuninit()
-
 
 #Close SLM SDK
 Meadowlark.closesdk()
